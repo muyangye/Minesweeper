bd0b026e414428fd0d0421adabf16d307674f44c
muyangye <muyangye@usc.edu>
2022-09-14 02:26:04 -0700

    Add check win/lose logic

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.icu.number.IntegerWidth;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private int[][] mines = new int[10][8];
     private int[][] flags = new int[10][8];
     private TextView[][] grids = new TextView[10][8];
+    private boolean isPick = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Add 10 * 8 grids dynamically
         GridLayout gameBoard = findViewById(R.id.gameBoard);
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
                 TextView tv = new TextView(this);
-                tv.setHeight(dpToPixel(40));
-                tv.setWidth(dpToPixel(40));
+                tv.setHeight(dpToPixel(35));
+                tv.setWidth(dpToPixel(35));
                 tv.setTextSize( 32 );//dpToPixel(32) );
                 tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
                 tv.setBackgroundColor(Color.GREEN);
@@ -64,9 +64,8 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         List<Integer> minesPos = random.ints(0, 80)
                 .distinct().limit(10).boxed().collect(Collectors.toList());
-        for (Integer minePos : minesPos)
-        {
-            mines[minePos / 8][minePos / 10] = 1;
+        for (Integer minePos : minesPos) {
+            mines[minePos % 10][minePos % 8] = 1;
         }
     }
 
@@ -75,15 +74,12 @@ public class MainActivity extends AppCompatActivity {
         return Math.round(dp * density);
     }
 
-    // helper function to get the "coordinate" (i, j) of the current TextView
+    // Helper function to get the "coordinate" (i, j) of the current TextView
     private Pair<Integer, Integer> findCoordinate(TextView tv)
     {
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                if (grids[i][j] == tv)
-                {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (grids[i][j] == tv) {
                     return new Pair<Integer, Integer>(i, j);
                 }
             }
@@ -91,60 +87,133 @@ public class MainActivity extends AppCompatActivity {
         return new Pair<Integer, Integer>(-1, -1);
     }
 
+    private List<Pair<Integer, Integer>> findMinesCoordinates()
+    {
+        List<Pair<Integer, Integer>> minesCoordinates = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (mines[i][j] == 1) {
+                    minesCoordinates.add(new Pair<>(i, j));
+                }
+            }
+        }
+        return minesCoordinates;
+    }
+
+    private List<Pair<Integer, Integer>> findFlagsCoordinates()
+    {
+        List<Pair<Integer, Integer>> flagsCoordinates = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (flags[i][j] == 1) {
+                    flagsCoordinates.add(new Pair<>(i, j));
+                }
+            }
+        }
+        return flagsCoordinates;
+    }
+
     public void onClickTV(View view)
     {
         TextView tv = (TextView) view;
         Pair<Integer, Integer> startCoordinate = findCoordinate(tv);
-        if (mines[startCoordinate.first.intValue()][startCoordinate.second.intValue()] == 1)
-        {
-            System.out.println("BOMB!!!");
-            return;
-        }
-        // BFS
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<Pair<Integer, Integer>>();
-        queue.add(startCoordinate);
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
-        int[][] directions = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
-        while (!queue.isEmpty())
-        {
-            for (int i = 0; i < queue.size(); i++)
-            {
-                Pair<Integer, Integer> coordinate = queue.poll();
-                int row = coordinate.first;
-                int col = coordinate.second;
-                grids[row][col].setBackgroundColor(Color.GRAY);
-                List<Pair<Integer, Integer>> okCoordinates = new ArrayList<>();
-                int maxOkCoordinates = 0;
-                for (int[] direction : directions)
-                {
-                    int nextRow = direction[0] + row;
-                    int nextCol = direction[1] + col;
-                    if (nextRow >= 0 && nextRow < 10 && nextCol >=0 && nextCol < 8)
-                    {
-                        maxOkCoordinates += 1;
-                        if (mines[nextRow][nextCol] != 1)
-                        {
-                            okCoordinates.add(new Pair<Integer, Integer>(nextRow, nextCol));
+        if (isPick) {
+            // If click on a mine
+            if (mines[startCoordinate.first.intValue()][startCoordinate.second.intValue()] == 1) {
+                // Display all mines
+                for (Pair<Integer, Integer> mineCoordinate : findMinesCoordinates()) {
+                    int row = mineCoordinate.first;
+                    int col = mineCoordinate.second;
+                    grids[row][col].setText(R.string.mine);
+                }
+                // Redirect to result page and show lost message
+                Intent intent = new Intent(this, ResultActivity.class);
+                startActivity(intent);
+                return;
+            }
+            // BFS
+            Queue<Pair<Integer, Integer>> queue = new LinkedList<Pair<Integer, Integer>>();
+            queue.add(startCoordinate);
+            Set<Pair<Integer, Integer>> visited = new HashSet<>();
+            int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+            while (!queue.isEmpty()) {
+                for (int i = 0; i < queue.size(); i++) {
+                    Pair<Integer, Integer> coordinate = queue.poll();
+                    int row = coordinate.first;
+                    int col = coordinate.second;
+                    grids[row][col].setBackgroundColor(Color.GRAY);
+                    List<Pair<Integer, Integer>> okCoordinates = new ArrayList<>();
+                    // Total number of possible nearby grids that aren't miness
+                    // (this variable is to handle corners that don't count towards mines/safe)
+                    int maxOkCoordinates = 0;
+                    for (int[] direction : directions) {
+                        int nextRow = direction[0] + row;
+                        int nextCol = direction[1] + col;
+                        if (nextRow >= 0 && nextRow < 10 && nextCol >= 0 && nextCol < 8) {
+                            maxOkCoordinates += 1;
+                            if (mines[nextRow][nextCol] != 1) {
+                                okCoordinates.add(new Pair<Integer, Integer>(nextRow, nextCol));
+                            }
                         }
                     }
-                }
-                if (okCoordinates.size() != maxOkCoordinates)
-                {
-                    grids[row][col].setText(String.valueOf(maxOkCoordinates - okCoordinates.size()));
-                }
-                else
-                {
-                    for (Pair<Integer, Integer> okCoordinate : okCoordinates)
-                    {
-                        if (!visited.contains(okCoordinate))
-                        {
-                            queue.add(okCoordinate);
-                            visited.add(okCoordinate);
+                    // There are mines nearby
+                    if (okCoordinates.size() != maxOkCoordinates) {
+                        // Hint the user how many
+                        grids[row][col].setText(String.valueOf(maxOkCoordinates - okCoordinates.size()));
+                    }
+                    // All nearby grids are not mines
+                    else {
+                        // Add them to BFS queue
+                        for (Pair<Integer, Integer> okCoordinate : okCoordinates) {
+                            if (!visited.contains(okCoordinate)) {
+                                queue.add(okCoordinate);
+                                visited.add(okCoordinate);
+                            }
                         }
                     }
                 }
             }
         }
+        else {
+            int row = startCoordinate.first;
+            int col = startCoordinate.second;
+            if (((ColorDrawable) grids[row][col].getBackground()).getColor() == Color.GRAY) {
+                return;
+            }
+            if (flags[row][col] == 1) {
+                grids[row][col].setText("");
+                flags[row][col] = 0;
+            }
+            else {
+                grids[row][col].setText(R.string.flag);
+                flags[row][col] = 1;
+                // If the user wins
+                if (isWin()) {
+                    // Redirect to result page
+                    Intent intent = new Intent(this, ResultActivity.class);
+                    startActivity(intent);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void changeMode(View view)
+    {
+        Button button = findViewById(R.id.mode);
+        if (isPick) {
+            isPick = false;
+            button.setText(R.string.flag);
+        }
+        else {
+            isPick = true;
+            button.setText(R.string.pick);
+        }
+    }
+
+    // Check if the user wins
+    public boolean isWin() {
+        return findMinesCoordinates().equals(findFlagsCoordinates());
     }
 
     public void showResult()
